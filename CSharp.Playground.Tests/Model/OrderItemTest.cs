f581be88d07bb6e32f3f181ea6d6ccf69f9e08af
rufer7 <marc.rufer@d-fens.net>
2015-07-21 16:00:37 +0200

    minor

--- CSharp.Playground.Tests/Model/OrderItemTest.cs
+++ CSharp.Playground.Tests/Model/OrderItemTest.cs
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 using CSharp.Playground.Tests.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
