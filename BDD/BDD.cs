1ac223c960258755a00bed073508c55b53a0ed5c
rufer7 <marc.rufer@d-fens.net>
2015-07-22 13:57:48 +0200

    #1

---
+++ BDD/BDD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD
+{
+    public class BDD
+    {
+        public static Int32 getTheAnswer()
+        {
+            return 42;
+        }
+    }
+}
