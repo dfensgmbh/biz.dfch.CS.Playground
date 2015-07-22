cacb8b7ad0418de0708f2f9f99d0c1c25f83fbe6
rufer7 <marc.rufer@d-fens.net>
2015-07-22 13:57:35 +0200

    resolves #2

---
+++ BDD.Tests/BDDTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BDD.Tests
+{
+    [TestClass]
+    public class BDDTest
+    {
+        [TestMethod]
+        public void TheAnswerToEverythingReturns42()
+        {
+            Assert.AreEqual(42, BDD.getTheAnswer());
+        }
+    }
+}
