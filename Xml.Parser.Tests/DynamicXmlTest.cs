f02209a5c167a383b17870cfa098b94f54c93957
rufer7 <marc.rufer@d-fens.net>
2015-09-01 08:55:24 +0200

    added dynamic xml deserialization example

---
+++ Xml.Parser.Tests/DynamicXmlTest.cs
@@ -0,0 +1,48 @@
+﻿/**
+ * Copyright 2015 Marc Rufer, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xml.Parser.Tests
+{
+    [TestClass]
+    public class DynamicXmlTest
+    {
+        [TestMethod]
+        public void DynamicDeserializationTest()
+        {
+            dynamic result = DynamicXml.Parse(CreateSampleXml());
+            var id = result.Student[0].ID;
+            Assert.AreEqual("100", result.Student[0].ID);
+            Assert.AreEqual("Lily", result.Student[1].Name);
+        }
+
+        private String CreateSampleXml()
+        {
+            return@"<Students>
+                <Student ID=""100"">
+                    <Name>Paul</Name>
+                    <Mark>90</Mark>
+                </Student>
+                <Student>
+                    <Name>Lilly</Name>
+                    <Mark>80</Mark>
+                </Student>
+            </Students>";
+        }
+    }
+}
---
+++ Xml.Parser/DynamicXml.cs
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2015 Marc Rufer, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+﻿using System.Dynamic;
+﻿using System.Linq;
+﻿using System.Xml.Linq;
+
+namespace Xml.Parser
+{
+    public class DynamicXml : DynamicObject
+    {
+        XElement _root;
+        private DynamicXml(XElement root)
+        {
+            _root = root;
+        }
+
+        public static DynamicXml Parse(string xmlString)
+        {
+            return new DynamicXml(XDocument.Parse(xmlString).Root);
+        }
+
+        public static DynamicXml Load(string filename)
+        {
+            return new DynamicXml(XDocument.Load(filename).Root);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = null;
+
+            var att = _root.Attribute(binder.Name);
+            if (att != null)
+            {
+                result = att.Value;
+                return true;
+            }
+
+            var nodes = _root.Elements(binder.Name);
+            if (nodes.Count() > 1)
+            {
+                result = nodes.Select(n => new DynamicXml(n)).ToList();
+                return true;
+            }
+
+            var node = _root.Element(binder.Name);
+            if (node != null)
+            {
+                if (node.HasElements)
+                {
+                    result = new DynamicXml(node);
+                }
+                else
+                {
+                    result = node.Value;
+                }
+                return true;
+            }
+
+            return true;
+        }
+    }
+}
