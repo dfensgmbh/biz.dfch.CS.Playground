6a220f2f342473dcbdfe056699de0a86e2496871
rufer7 <rufermarc@gmail.com>
2018-09-05 14:56:01 +0200

    Projects added - MyConsoleApp - MyWpfUserControlLibrary

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWpfUserControlLibrary;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            RunApplication();
+        }
+
+        /**
+         * Starting a WPF Window, which is in a WPF user control library, from another non WPF executable
+         */
+        private static void RunApplication()
+        {
+            //CustomApplication app = new CustomApplication();
+            //app.Run();
+
+            var myWindow = new MyWindow();
+            myWindow.ShowDialog();
+        }
+    }
+}
